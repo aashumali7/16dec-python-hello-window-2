6e054df64265bc5c355488b2f793ed87d28548c0
aashumali <aashishmali223@gmail.com>
2023-12-16 11:27:59 +0530

    new

---
+++ window.py
@@ -0,0 +1,9 @@
+#import area
+import sys
+#from topmodule.submodule import element1,element2,element3
+from PyQt6.QtWidgets import QApplication,QWidget
+
+aashu = QApplication([]) #passing a empty list
+aashish = QWidget() #co = ClassName
+aashish.show() #co.method
+aashu.exec() #co.method
